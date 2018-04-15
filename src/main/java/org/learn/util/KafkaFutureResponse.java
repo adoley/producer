6d27db5c087093ed4ad8126d01906ab9d18fe3dd
Apollo Doley <apollodoley3@gmail.com>
2018-04-15 15:03:20 +0530

    added logger

--- src/main/java/org/learn/util/KafkaFutureResponse.java
+++ src/main/java/org/learn/util/KafkaFutureResponse.java
@@ -2,13 +2,14 @@ package org.learn.util;
 
 import org.apache.kafka.clients.producer.Callback;
 import org.apache.kafka.clients.producer.RecordMetadata;
+import org.apache.log4j.Logger;
 
 public class KafkaFutureResponse implements Callback {
+    final static Logger logger = Logger.getLogger(KafkaFutureResponse.class);
     @Override
     public void onCompletion(RecordMetadata recordMetadata, Exception e) {
         if(e!=null){
-            //ToDo implement log
-            e.printStackTrace();
+            logger.error(e);
         }
     }
 }
